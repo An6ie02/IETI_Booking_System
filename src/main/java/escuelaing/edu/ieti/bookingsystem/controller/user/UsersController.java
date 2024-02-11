3e531cc8773476fb99c4e1164ab027ba63d6fcd9
Angie Mojica <nataliamojica90@gmail.com>
2024-02-10 23:50:33 -0500

    Controlador de usuarios

---
+++ src/main/java/escuelaing/edu/ieti/bookingsystem/controller/user/UsersController.java
@@ -0,0 +1,80 @@
+package escuelaing.edu.ieti.bookingsystem.controller.user;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import escuelaing.edu.ieti.bookingsystem.exception.UserNotFoundException;
+import escuelaing.edu.ieti.bookingsystem.repository.user.User;
+import escuelaing.edu.ieti.bookingsystem.repository.user.UserDto;
+import escuelaing.edu.ieti.bookingsystem.service.user.UsersService;
+
+@RestController
+@RequestMapping(value = "/v1/users")
+public class UsersController {
+
+    private final UsersService usersService;
+
+    public UsersController(@Autowired UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDto userDto) {
+        User user = usersService.save(new User(userDto));
+        URI createdUserUri = URI.create("/v1/users/" + user.getId());
+        return ResponseEntity.created(createdUserUri).body(user);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = usersService.all();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> findById(@PathVariable("id") String id) {
+        Optional<User> user = usersService.findById(id);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        } else {
+            throw new UserNotFoundException(id);
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") String id, @RequestBody UserDto userDto) {
+        Optional<User> optionalUser = usersService.findById(id);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.update(userDto);
+            User updatedUser = usersService.save(user);
+            return ResponseEntity.ok(updatedUser);
+        } else {
+            throw new UserNotFoundException(id);
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") String id) {
+        if (usersService.findById(id).isPresent()) {
+            usersService.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            throw new UserNotFoundException(id);
+        }
+
+    }
+}
---
+++ src/main/java/escuelaing/edu/ieti/bookingsystem/service/user/UsersServiceMap.java
@@ -0,0 +1,55 @@
+package escuelaing.edu.ieti.bookingsystem.service.user;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import escuelaing.edu.ieti.bookingsystem.exception.UserNotFoundException;
+import escuelaing.edu.ieti.bookingsystem.repository.user.User;
+
+@Service
+public class UsersServiceMap implements UsersService {
+
+    Map<String, User> users = new HashMap<>();
+
+    @Override
+    public User save(User user) {
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(String id) {
+        if (users.containsKey(id)) {
+            return Optional.of(users.get(id));
+        } else {
+            throw new UserNotFoundException(id);
+        }
+    }
+
+    @Override
+    public List<User> all() {
+        List<User> usersList = new ArrayList<>();
+        usersList.addAll(users.values());
+        return usersList;
+    }
+
+    @Override
+    public void deleteById(String id) {
+        if (!users.containsKey(id))
+            throw new UserNotFoundException(id);
+        users.remove(id);
+    }
+
+    @Override
+    public User update(User user, String userId) {
+        if (!users.containsKey(userId))
+            throw new UserNotFoundException(userId);
+        users.put(userId, user);
+        return user;
+    }
+}
